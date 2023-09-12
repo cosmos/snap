efe947394039e68c93657169470888f1ce637a0e
Joe S <schnetzlerjoe@gmail.com>
2023-09-11 21:15:00 -0400

    change adapter

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -1,19 +1,8 @@
-//import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: vercel()
-	}
+export default {
+    kit: {
+        adapter: adapter()
+    }
 };
-
-export default config;
