29ab2d1e7e5cda5839374c6cb8eecccee8d76979
Joe Schnetzler <schnetzlerjoe@gmail.com>
2023-09-12 17:15:19 +0000

    Add new pages

--- packages/ui/src/components/Menu.svelte
+++ packages/ui/src/components/Menu.svelte
@@ -5,8 +5,9 @@
     let menu_items = [
         { route: "/balances", title: "Balances", icon: "https://anima-uploads.s3.amazonaws.com/projects/64863aebc1255e7dd4fb600b/releases/64a70dda287bc6479f0ac9fd/img/dashboard.svg", path: "/balances" },
         { route: "/transactions", title: "History", icon: "https://anima-uploads.s3.amazonaws.com/projects/64863aebc1255e7dd4fb600b/releases/64a70dda287bc6479f0ac9fd/img/dns.svg", path: "/transactions" },
-        { route: "/address", title: "Address Book", icon: "https://anima-uploads.s3.amazonaws.com/projects/64863aebc1255e7dd4fb600b/releases/64a70dda287bc6479f0ac9fd/img/account-box.svg", path: "/address" },
-        { route: "/funds", title: "Funds", icon: "https://anima-uploads.s3.amazonaws.com/projects/64863aebc1255e7dd4fb600b/releases/64a70dda287bc6479f0ac9fd/img/account-box.svg", path: "/funds" },
+        { route: "/address", title: "Address Book", icon: "M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4", path: "/address" },
+        { route: "/swap", title: "Swap", icon: "M16 1v5h-5M2 19v-5h5m10-4a8 8 0 0 1-14.947 3.97M1 10a8 8 0 0 1 14.947-3.97", path: "/swap" },
+        { route: "/fiat", title: "Fiat", icon: "https://anima-uploads.s3.amazonaws.com/projects/64863aebc1255e7dd4fb600b/releases/64a70dda287bc6479f0ac9fd/img/account-box.svg", path: "/fiat" },
         { route: "/settings", title: "Settings", icon: "https://anima-uploads.s3.amazonaws.com/projects/64863aebc1255e7dd4fb600b/releases/64a70dda287bc6479f0ac9fd/img/settings.svg", path: "/settings" }
     ]
     let active = $page.route.id;
@@ -22,11 +23,9 @@
           <!-- svelte-ignore a11y-click-events-have-key-events -->
           <div on:click={() => goto(tab.path)} class="w-full flex justify-between h-[40px] items-center cursor-pointer">
             <div class="frame">
-                <img
-                class="item"
-                src={tab.icon}
-                alt={tab.title.toLowerCase()}
-                />
+                <svg class="item w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={tab.icon}/>
+                </svg>
                 <div class="item-1 inter-medium-white-16px">{tab.title}</div>
             </div>
             <div hidden={active != tab.route} class="rectangle-12"></div>
@@ -64,8 +63,8 @@
 }
 
 .item {
-  height: 20px;
-  min-width: 20px;
+  height: 18px;
+  min-width: 18px;
   position: relative;
 }
 
---
+++ packages/ui/src/routes/swap/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<div style="padding: 25px;">
+    <div class="">
+        <div class="w-full h-full flex justify-center items-center">
+            <div class="mt-6 w-full h-full flex justify-center items-center h-[720px] w-[420px]"> 
+                <iframe
+                    title="squid_widget"
+                    width="100%"
+                    height="100%"
+                    src="https://widget.squidrouter.com/iframe?config=%7B%22integratorId%22%3A%22squid-swap-widget%22%2C%22companyName%22%3A%22Custom%22%2C%22style%22%3A%7B%22neutralContent%22%3A%22%23ffffff%22%2C%22baseContent%22%3A%22%23ffffff%22%2C%22base100%22%3A%22%23000000%22%2C%22base200%22%3A%22%23000000%22%2C%22base300%22%3A%22%23594bff%22%2C%22error%22%3A%22%23ED6A5E%22%2C%22warning%22%3A%22%23FFB155%22%2C%22success%22%3A%22%232EAEB0%22%2C%22primary%22%3A%22%23ebebeb%22%2C%22secondary%22%3A%22%23594bff%22%2C%22secondaryContent%22%3A%22%23F6F7FB%22%2C%22neutral%22%3A%22%23000000%22%2C%22roundedBtn%22%3A%2210px%22%2C%22roundedCornerBtn%22%3A%2210px%22%2C%22roundedBox%22%3A%2210px%22%2C%22roundedDropDown%22%3A%2210px%22%7D%2C%22slippage%22%3A1.5%2C%22infiniteApproval%22%3Afalse%2C%22enableExpress%22%3Atrue%2C%22apiUrl%22%3A%22https%3A%2F%2Fapi.squidrouter.com%22%2C%22mainLogoUrl%22%3A%22%22%2C%22comingSoonChainIds%22%3A%5B%5D%2C%22titles%22%3A%7B%22swap%22%3A%22Swap%22%2C%22settings%22%3A%22Settings%22%2C%22wallets%22%3A%22Wallets%22%2C%22tokens%22%3A%22Select%20Token%22%2C%22chains%22%3A%22Select%20Chain%22%2C%22history%22%3A%22History%22%2C%22transaction%22%3A%22Transaction%22%2C%22allTokens%22%3A%22Select%20Token%22%2C%22destination%22%3A%22Destination%20address%22%7D%2C%22priceImpactWarnings%22%3A%7B%22warning%22%3A3%2C%22critical%22%3A5%7D%2C%22showOnRampLink%22%3Atrue%7D"
+                /> 
+            </div>
+        </div> 
+    </div>
+</div>
+
+<style>
+</style>
