b33d51021a1295d268670e44d225b2c2bdb9d862
Joe Schnetzler <schnetzlerjoe@gmail.com>
2023-09-13 18:45:48 +0000

    Edit swaps

--- packages/ui/src/routes/fiat/+page.svelte
+++ packages/ui/src/routes/fiat/+page.svelte
@@ -6,7 +6,7 @@
         <div class="h-full flex justify-center items-center overflow-hidden">
             <!-- svelte-ignore a11y-missing-attribute -->
             <div class="mt-6 w-full h-full flex justify-center items-center h-[750px] max-w-[420px] overflow-hidden">
-                <iframe src="https://app.kado.money/?apiKey=b93b823e-4b68-4fac-88d9-f3551d7b9a15&productList=BUY,SELL&theme=dark" width="100%" height="100%" style="border: 0px; display: block; background-color: transparent;" scrolling="no"></iframe>
+                <iframe src="https://app.kado.money/?apiKey=b93b823e-4b68-4fac-88d9-f3551d7b9a15&productList=BUY,SELL&theme=dark" width="100%" height="750px" style="border: 0px; display: block; background-color: transparent;"></iframe>
             </div>
         </div> 
     </div>
