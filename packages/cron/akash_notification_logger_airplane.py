8a0167ea0d0a00e5a750e0ac77966297b0329119
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2023-10-24 19:39:23 -0400

    define events

--- packages/cron/akash_notification_logger_airplane.py
+++ packages/cron/akash_notification_logger_airplane.py
@@ -1,4 +1,22 @@
 import airplane
+import json
+
+
+def get_lease_low_balance_event(address: str, lease_id: str, lease_name: str):
+    return json.dumps({
+        "read" : False,
+        "address" : address,
+        "lease" : lease_id,
+        "notification": f"Lease balance is below $1 for lease {lease_name}. Refill as soon as possible.",
+    })
+
+def get_lease_shut_down_event(address: str, lease_id: str, lease_name: str):
+    return json.dumps({
+        "read" : False,
+        "address" : address,
+        "lease" : lease_id,
+        "notification": f"Lease shut down for {lease_name}. Relaunch as soon as possible to limit downtime.",
+    })
 
 
 @airplane.task(
@@ -6,7 +24,19 @@ import airplane
     name="Akash Notification Logger",
     description="Scrapes and logs script that takes Akash Network events we are looking for and adds them as unread notifications to the MongoDB DB to use in the MetaMask Snap UI later.",
 )
-def akash_notification_logger():
+def akash_notification_logger(resources=[
+    # Attach the resource with slug "backend_db" under the alias "db"
+        airplane.Resource(
+            slug="akshay_mongodb",
+        )
+    ],
+    schedules=[
+        airplane.Schedule(
+            slug="akash_notification_logger",
+            cron="*/15 * * * *",
+            description="checks for notification every 15 minutes",
+        )
+    ]):
     data = [
         {"id": 1, "name": "Gabriel Davis", "role": "Dentist"},
         {"id": 2, "name": "Carolyn Garcia", "role": "Sales"},
