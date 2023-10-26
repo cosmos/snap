25c8fb547ba88ff2adfdc1be5df6d71d78927a20
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2023-10-26 19:59:28 -0400

    implement event logic

--- packages/cron/akash_notification_logger_airplane.py
+++ packages/cron/akash_notification_logger_airplane.py
@@ -1,10 +1,9 @@
 import airplane
 import json
+import requests
 
-from database import get_database
-from typing import Optional
+api_url = "https://rest.cosmos.directory/akash/akash/deployment/v1beta3/deployments/list"
 
-get_database()
 
 def get_lease_low_balance_event(address: str, lease_id: str, lease_name: str):
     return json.dumps({
@@ -22,6 +21,57 @@ def get_lease_shut_down_event(address: str, lease_id: str, lease_name: str):
         "notification": f"Lease shut down for {lease_name}. Relaunch as soon as possible to limit downtime.",
     })
 
+def get_events_to_notify(deployments):
+    events = []
+
+    for item in deployments:
+        if item['escrow_account']['balance']['amount'] < 1:
+            events.append(get_lease_low_balance_event())
+    """
+    TODO 
+    Implement shutdown logic
+    
+    """
+        
+    return events
+
+
+def get_events():
+    events_to_notify = []
+
+    current_offset = 0
+    page_limit = 1000
+
+    while True:
+
+        params = {
+            "filters.state": "active",
+            "pagination.limit": page_limit,
+            "pagination.count_total": True,
+            "pagination.offset": current_offset
+        }  
+
+        response = requests.get(api_url, params=params)
+
+        if response.status_code == 200:
+
+            data = response.json()
+
+            events_to_notify += get_leases_to_notify(data['deployments'])
+
+            pagination_total = data['pagination']['total']
+            current_offset += page_limit
+
+            if current_offset >= pagination_total:
+                break
+        
+        else:
+            print(f"API call to cosmos failed with status code: {response.status_code}")
+            break
+    
+    return events_to_notify
+
+
 
 @airplane.task(
     slug="akash_notification_logger",
@@ -34,29 +84,16 @@ def get_lease_shut_down_event(address: str, lease_id: str, lease_name: str):
     ],
     schedules = [
         airplane.Schedule(
-            slug="akash_notification_logger",
+            slug="akash_notification_logger_cron",
             cron="*/15 * * * *",
-            description="checks for notification every 15 minutes",
+            description="checks for notifications every 15 minutes",
         )
     ]
 )
-def akash_notification_logger(address: str,
-    lease_id: str,
-    lease_name: str,
-    is_low_balance_event: bool,
-    is_shut_down_event: bool):
+def akash_notification_logger():
     
-    events = []
-
-    if is_low_balance_event:
-        events.append(get_lease_low_balance_event(address, lease_id, lease_name))
+    events = get_events()
     
-    if is_shut_down_event:
-        events.append(get_lease_shut_down_event(address, lease_id, lease_name))
+    run = airplane.mongodb.insert_many('akshay_mongodb', 'snap_notifications', events)
 
-    
-
-    airplane.mongodb.insert_many("akshay_mongodb", "akash_notifications", events)
-
-
-    return events
+    return run.output
