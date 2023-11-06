1a2475e003d9e4b557aa4dc235d0436f3a93415e
Joe Schnetzler <schnetzlerjoe@gmail.com>
2023-11-06 15:28:11 -0500

    New swap

---
+++ packages/ui/src/components/Swap.svelte
@@ -0,0 +1,94 @@
+<script>
+</script>
+
+<div class="w-full max-w-[700px] p-[20px] font-inter bg-[#14141440] rounded-3xl border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100">
+    <div class="w-full p-4">
+        <div class="w-full">
+            <div class="font-bold text-[20px] text-white text-lg tracking-tight leading-normal whitespace-nowrap">
+                Swap
+            </div>
+        </div>
+        <div class="w-full mt-4">
+            <div class="">
+                <div class="w-full mb-4">
+                    <div class="w-full bg-[#14141480] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-4">
+                        <div class="opacity-25 font-medium text-white text-xs text-right">
+                            Available balance: 0
+                        </div>
+                        <div class="font-medium text-white text-base tracking-tight leading-normal">
+                            From
+                        </div>
+                        <div class="flex justify-between items-center mt-4">
+                            <div class="bg-[#14141473] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-2 w-1/2 mr-2">
+                                <div class="opacity-25 font-medium text-white text-sm">
+                                    Select chain
+                                </div>
+                            </div>
+                            <div class="bg-[#14141473] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-2 w-1/2 ml-2">
+                                <div class="opacity-25 font-medium text-white text-sm">
+                                    Select coin
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-4">
+                            <div class="bg-[#14141473] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-2">
+                                <div class="opacity-25 font-medium text-white text-sm">
+                                    Enter amount
+                                </div>
+                                <div class="cursor-pointer absolute top-2 right-4 font-semibold text-[#594bff] text-sm text-right">
+                                    MAX
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="absolute w-10 h-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <div class="bg-[#594bff] rounded-lg">
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full bg-[#14141480] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-4">
+                        <div class="opacity-25 font-medium text-white text-xs text-right">
+                            Available balance: 0
+                        </div>
+                        <div class="font-medium text-white text-base tracking-tight leading-normal">
+                            To
+                        </div>
+                        <div class="flex justify-between items-center mt-4">
+                            <div class="bg-[#14141473] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-2 w-1/2 mr-2">
+                                <div class="opacity-25 font-medium text-white text-sm">
+                                    Select chain
+                                </div>
+                            </div>
+                            <div class="bg-[#14141473] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-2 w-1/2 ml-2">
+                                <div class="opacity-25 font-medium text-white text-sm">
+                                    Select coin
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-4">
+                            <div class="bg-[#14141473] rounded-lg border border-solid border-[#ffffff1a] backdrop-blur-lg backdrop-brightness-100 p-2">
+                                <div class="opacity-25 font-medium text-white text-sm">
+                                    Enter amount
+                                </div>
+                                <div class="cursor-pointer absolute top-2 right-4 font-semibold text-[#594bff] text-sm text-right">
+                                    MAX
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="cursor-pointer h-[45px] mt-[20px] w-full flex items-center justify-center bg-[#594bff] rounded-lg overflow-hidden">
+            <div class="font-medium text-white text-sm tracking-tight leading-normal">
+                Swap
+            </div>
+        </button>
+    </div>
+</div>
+<style lang="scss">
+    button {
+        @apply active:scale-95 transition-all duration-300 cursor-pointer;
+    }
+</style>
--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -18,7 +18,10 @@ module.exports = {
 
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
-    }
+    },
+    fontFamily: {
+      inter: 'var(--font-family-inter)',
+    },
   },
   plugins: [],
 }
