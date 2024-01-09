da6af0974ce0ab661f7503dfcace826b00c27dce
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2024-01-09 15:01:42 -0500

    appwrite db calls

--- packages/cron/akash_notification_logger.py
+++ packages/cron/akash_notification_logger.py
@@ -3,6 +3,14 @@ import os
 from appwrite.client import Client
 from appwrite.services import databases
 from appwrite.services import functions
+from appwrite.query import Query
+from pydantic import BaseModel
+
+
+class DB_DATA(BaseModel):
+    total: int
+    documents: list
+
 
 client = Client()
 db = databases.Databases(client)
@@ -10,10 +18,11 @@ funcs = functions.Functions(client)
 
 if os.environ.get("APPWRITE_ENDPOINT") is None:
     raise EnvironmentError('The environment variable APPWRITE_ENDPOINT is not set.')
+
 client.set_endpoint('https://cloud.appwrite.io/v1').set_project('659832bdd99000571f19').set_key(os.environ.get("APPWRITE_KEY")).set_self_signed()
 
 # Constants for MongoDB resources
-MONGODB_RESOURCE = 'akash_mongodb'
+RESOURCE = 'akash_mongodb'
 OPEN_LEASE_COLLECTION_NAME = 'open_leases'
 NOTIFICATIONS_COLLECTION_NAME = 'notifications'
 
@@ -27,9 +36,10 @@ def get_lease_shut_down_events(current_open_leases):
     # Initialize an empty list to store shutdown events
     events = []
 
-    # Fetch the previously open leases from MongoDB
-    db_data = airplane.mongodb.find(MONGODB_RESOURCE, OPEN_LEASE_COLLECTION_NAME, filter={"state": "open"})
-    old_leases = db_data.output
+    # Fetch the previously open leases from database
+    data = db.list_documents(RESOURCE, OPEN_LEASE_COLLECTION_NAME, Query.equal('state', 'open'))
+    db_data = DB_DATA(data['total'], data['documents'])
+    old_leases = db_data.documents
 
     closed_leases = [] # stores leases that were logged before and are now closed
     prev_open_leases = [] # stores leases that were logged before and are still open
@@ -37,7 +47,7 @@ def get_lease_shut_down_events(current_open_leases):
 
     # Compare the current open leases with the previous ones to determine if any have been closed
     for lease in old_leases:
-        if lease not in current_open_leases:
+        if current_open_leases.get(lease['lease_id']):
             # If a lease has been shut down, mark its state as "closed" and record the event
             closed_leases.append(lease['lease_id'])
             events.append({
@@ -48,34 +58,40 @@ def get_lease_shut_down_events(current_open_leases):
                         })
         else:
             # record previously logged leases
-            prev_open_leases.append(lease)
+            prev_open_leases.append(lease['lease_id'])
     
 
     # Update the leases that are closed now
-    airplane.mongodb.update_many(MONGODB_RESOURCE, OPEN_LEASE_COLLECTION_NAME,
-                                 update={'$set': {'state': 'closed'}},
-                                 filter={'lease_id': {'$in': closed_leases}}
-                                )
+    for lease in closed_leases:
+        db.update_document(RESOURCE, OPEN_LEASE_COLLECTION_NAME, lease, {
+             'state' : 'closed',
+             'lease_id' : lease,
+        })
+
     # Get New Leases
-    new_leases = [lease for lease in current_open_leases if lease not in prev_open_leases]
+    new_leases = [lease for lease in list(current_open_leases.keys()) if lease not in prev_open_leases]
     
     # Insert New leases
-    airplane.mongodb.insert_many(MONGODB_RESOURCE, OPEN_LEASE_COLLECTION_NAME, new_leases)
+    for lease in new_leases:
+        db.create_document(RESOURCE, OPEN_LEASE_COLLECTION_NAME, lease, {
+             'state' : 'open',
+             'lease_id' : lease,
+        })
 
     return events
 
 def get_lease_low_balance_events(deployments):
     # Initialize lists to store low balance events and current open leases
     events = []
-    current_open_leases = []
+    current_open_leases = dict()
 
     # Process each deployment item
     for item in deployments:
         # Record all the current open leases
-        current_open_leases.append({
+        current_open_leases[item['escrow_account']['id']['xid']] = {
                                     "lease_id": item['escrow_account']['id']['xid'],
                                     "state": item['deployment']['state']
-                                    })
+                                    }
         # check for low balance
         if float(item['escrow_account']['balance']['amount']) < 1:
             events.append({
@@ -94,7 +110,7 @@ def get_events():
     try:
         # Initialize lists to store notifications
         events_to_notify = []
-        current_open_leases = []
+        current_open_leases = dict()
 
         # Pagination variables for the API request
         current_offset = 0
@@ -128,7 +144,7 @@ def get_events():
 
                 # Accumulate events and open leases
                 events_to_notify += events
-                current_open_leases += open_leases
+                current_open_leases.update(open_leases)
 
                 #update pagination info
                 pagination_total = data['pagination']['total']
@@ -156,6 +172,7 @@ def akash_notification_logger():
     events = get_events()
 
     # Insert the events into the MongoDB notifications collection
-    airplane.mongodb.insert_many(MONGODB_RESOURCE, NOTIFICATIONS_COLLECTION_NAME, events)
+    for event in events:
+        db.create_document(RESOURCE, NOTIFICATIONS_COLLECTION_NAME, event['lease'], event)
 
     return "notifications uploaded to database successfully!"
