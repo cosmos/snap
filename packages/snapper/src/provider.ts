9f20da7c1a0b889b5791cecb0828e996e77a66c2
Joe Schnetzler <schnetzlerjoe@gmail.com>
2024-06-05 15:49:41 -0400

    Update snapper errors and dockerfile

--- packages/snapper/src/provider.ts
+++ packages/snapper/src/provider.ts
@@ -6,7 +6,7 @@ import { Address, Chain, CosmosAddress, Fees, Msg } from './types.js';
 import { DeliverTxResponse } from "@cosmjs/stargate";
 import { DEFAULT_SNAP_ID, addAddressToBook, deleteAddressFromBook, deleteChain, getAccountInfo, getAddressBook, getBech32Address, getBech32Addresses, getChains, getKey, installSnap, sendTx, sign, signAmino, signAndBroadcast, signDirect, suggestChain } from './snap.js';
 import { CosmJSOfflineSigner } from './signer.js';
-import { SignDoc } from 'cosmjs-types/cosmos/tx/v1beta1/tx.js';
+import { SignDoc } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
 import { Appwrite } from './appwrite.js';
 import { HttpEndpoint } from "@cosmjs/stargate";
 
