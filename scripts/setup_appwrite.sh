d71c389ae2d31239f1bdd1681c0c13746992e0f9
Joe S <schnetzlerjoe@gmail.com>
2024-08-28 14:39:34 -0400

    fix self host script

--- scripts/setup_appwrite.sh
+++ scripts/setup_appwrite.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # Check if necessary environment variables are set
-if [[ -z "$PROJECT_ID" ]]; then
-    echo "Error: PROJECT_ID is not set. Please set it to your Appwrite project ID."
+if [[ -z "$APPWRITE_ENDPOINT" ]]; then
+    echo "Error: APPWRITE_ENDPOINT is not set. Please set it to your Appwrite endpoint."
     exit 1
 fi
 
-if [[ -z "$ENDPOINT" ]]; then
-    echo "Error: ENDPOINT is not set. Please set it to your Appwrite endpoint."
+if [[ -z "$APPWRITE_PROJECT_ID" ]]; then
+    echo "Error: APPWRITE_PROJECT_ID is not set. Please set it to your Appwrite project ID."
     exit 1
 fi
 
-if [[ -z "$API_KEY" ]]; then
-    echo "Error: API_KEY is not set. Please set it to your Appwrite API key."
+if [[ -z "$APPWRITE_API_KEY" ]]; then
+    echo "Error: APPWRITE_API_KEY is not set. Please set it to your Appwrite API key."
     exit 1
 fi
+
+# Initialize Appwrite CLI
+appwrite client --endpoint "$APPWRITE_ENDPOINT" --projectId "$APPWRITE_PROJECT_ID" --key "$APPWRITE_API_KEY"
 
 # Function to create a collection
 create_collection() {
     COLLECTION_NAME=$1
-    RESPONSE=$(appwrite databases createCollection \
-        --projectId $PROJECT_ID \
-        --endpoint $ENDPOINT \
-        --key $API_KEY \
-        --databaseId "default" \
-        --collectionId $COLLECTION_NAME \
-        --name $COLLECTION_NAME)
-    echo $RESPONSE
+    appwrite databases create-collection \
+        --databaseId multisig \
+        --collectionId "$COLLECTION_NAME" \
+        --name "$COLLECTION_NAME" \
+        --permissions "create(\"any\")" "read(\"any\")" "update(\"any\")" "delete(\"any\")"
 }
 
 # Function to create a string attribute
@@ -35,17 +35,12 @@ create_string_attribute() {
     ATTRIBUTE=$2
     SIZE=$3
     REQUIRED=$4
-
-    RESPONSE=$(appwrite databases createStringAttribute \
-        --projectId $PROJECT_ID \
-        --endpoint $ENDPOINT \
-        --key $API_KEY \
-        --databaseId "default" \
-        --collectionId $COLLECTION_NAME \
-        --key $ATTRIBUTE \
-        --size $SIZE \
-        --required $REQUIRED)
-    echo $RESPONSE
+    appwrite databases create-string-attribute \
+        --databaseId multisig \
+        --collectionId "$COLLECTION_NAME" \
+        --key "$ATTRIBUTE" \
+        --size "$SIZE" \
+        --required "$REQUIRED"
 }
 
 # Function to create an enum attribute
@@ -55,18 +50,13 @@ create_enum_attribute() {
     ELEMENTS=$3
     REQUIRED=$4
     DEFAULT=$5
-
-    RESPONSE=$(appwrite databases createEnumAttribute \
-        --projectId $PROJECT_ID \
-        --endpoint $ENDPOINT \
-        --key $API_KEY \
-        --databaseId "default" \
-        --collectionId $COLLECTION_NAME \
-        --key $ATTRIBUTE \
-        --elements $ELEMENTS \
-        --required $REQUIRED \
-        --default $DEFAULT)
-    echo $RESPONSE
+    appwrite databases create-enum-attribute \
+        --databaseId multisig \
+        --collectionId "$COLLECTION_NAME" \
+        --key "$ATTRIBUTE" \
+        --elements "$ELEMENTS" \
+        --required "$REQUIRED" \
+        --default "$DEFAULT"
 }
 
 # Function to create an integer attribute
@@ -76,18 +66,13 @@ create_integer_attribute() {
     MIN=$3
     MAX=$4
     REQUIRED=$5
-
-    RESPONSE=$(appwrite databases createIntegerAttribute \
-        --projectId $PROJECT_ID \
-        --endpoint $ENDPOINT \
-        --key $API_KEY \
-        --databaseId "default" \
-        --collectionId $COLLECTION_NAME \
-        --key $ATTRIBUTE \
-        --min $MIN \
-        --max $MAX \
-        --required $REQUIRED)
-    echo $RESPONSE
+    appwrite databases create-integer-attribute \
+        --databaseId multisig \
+        --collectionId "$COLLECTION_NAME" \
+        --key "$ATTRIBUTE" \
+        --min "$MIN" \
+        --max "$MAX" \
+        --required "$REQUIRED"
 }
 
 # Function to create a relationship attribute
@@ -96,38 +81,32 @@ create_relationship_attribute() {
     ATTRIBUTE=$2
     RELATION_COLLECTION=$3
     RELATION_TYPE=$4
-
-    RESPONSE=$(appwrite databases createRelationshipAttribute \
-        --projectId $PROJECT_ID \
-        --endpoint $ENDPOINT \
-        --key $API_KEY \
-        --databaseId "default" \
-        --collectionId $COLLECTION_NAME \
-        --key $ATTRIBUTE \
-        --relatedCollectionId $RELATION_COLLECTION \
-        --type $RELATION_TYPE \
-        --required true)
-    echo $RESPONSE
+    appwrite databases create-relationship-attribute \
+        --databaseId multisig \
+        --collectionId "$COLLECTION_NAME" \
+        --relatedCollectionId "$RELATION_COLLECTION" \
+        --type "$RELATION_TYPE" \
+        --twoWay false \
+        --key "$ATTRIBUTE" \
+        --twoWayKey "parent_$ATTRIBUTE"
 }
 
 # Function to deploy an Appwrite function
 deploy_function() {
     FUNCTION_NAME=$1
     FUNCTION_PATH="./packages/appwrite/functions/$FUNCTION_NAME"
-    FUNCTION_FILE="$FUNCTION_PATH/function.json"
-
-    if [[ ! -f $FUNCTION_FILE ]]; then
-        echo "Error: $FUNCTION_FILE not found."
-        return 1
-    fi
-
-    RESPONSE=$(appwrite functions create \
-        --projectId $PROJECT_ID \
-        --endpoint $ENDPOINT \
-        --key $API_KEY \
-        --file $FUNCTION_FILE \
-        --name $FUNCTION_NAME)
-    echo $RESPONSE
+    appwrite functions create \
+        --functionId "$FUNCTION_NAME" \
+        --name "$FUNCTION_NAME" \
+        --runtime "node-18.0" \
+        --execute "any" \
+        --vars "APPWRITE_FUNCTION_ENDPOINT=$APPWRITE_ENDPOINT" "APPWRITE_FUNCTION_PROJECT_ID=$APPWRITE_PROJECT_ID" \
+        --entrypoint "src/index.js" \
+        --command "npm run start" \
+        --timeout 15
+    appwrite functions createDeployment \
+        --functionId "$FUNCTION_NAME" \
+        --code "$FUNCTION_PATH"
 }
 
 # Create Transactions collection
@@ -135,12 +114,12 @@ create_collection "transactions"
 
 # Create attributes for Transactions collection
 create_string_attribute "transactions" "chain_id" 1000 true
-create_enum_attribute "transactions" "status" "created" true "created"
-create_string_attribute "transactions" "signatures" 0 false
+create_enum_attribute "transactions" "status" "[\"created\"]" true "created"
+create_string_attribute "transactions" "signatures" 1073741824 false
 create_string_attribute "transactions" "body_bytes" 1073741824 true
 create_integer_attribute "transactions" "sequence" 0 100000 true
 create_string_attribute "transactions" "fee" 1073741824 true
-create_string_attribute "transactions" "messages" 0 false
+create_string_attribute "transactions" "messages" 1073741824 false
 
 # Create Multisig collection
 create_collection "multisig"
@@ -150,7 +129,7 @@ create_integer_attribute "multisig" "threshold" 0 1000 true
 create_string_attribute "multisig" "name" 1000 true
 create_string_attribute "multisig" "public_key" 1073741824 true
 create_string_attribute "multisig" "chain_id" 10000000 true
-create_string_attribute "multisig" "members" 0 false
+create_string_attribute "multisig" "members" 1073741824 false
 
 # Create relationship attribute for Multisig collection
 create_relationship_attribute "multisig" "transactions" "transactions" "oneToMany"
@@ -162,4 +141,4 @@ deploy_function "delete_multisig_tx"
 deploy_function "get_multisigs"
 deploy_function "sign_multisig_tx"
 
-echo "Appwrite setup successfully."
+echo "Appwrite setup successfully completed."
